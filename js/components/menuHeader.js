c304c029cce31f77dd833064018895e8b93e5d96
DDship123 <96617601+DDship123@users.noreply.github.com>
2025-07-14 12:50:40 +0700

    update url

--- js/components/menuHeader.js
+++ js/components/menuHeader.js
@@ -2,23 +2,23 @@ window.addEventListener("load", () => {
   let menuIframe = document.querySelector(".header .menu").contentDocument;
   let pageBtns = menuIframe.querySelectorAll(".menu_tags a");
   switch (window.location.href) {
-    case "/index.html":
+    case "https://www.tptrading.net/index.html":
       pageBtns[0].style.textDecoration = "underline";
       pageBtns[0].style.textUnderlineOffset = "5px";
       break;
-    case "/page/tradingBrokers.html":
+    case "https://www.tptrading.net/page/tradingBrokers.html":
       pageBtns[1].style.textDecoration = "underline";
       pageBtns[1].style.textUnderlineOffset = "5px";
       break;
-    case "/page/blog.html":
+    case "https://www.tptrading.net/page/blog.html":
       pageBtns[2].style.textDecoration = "underline";
       pageBtns[2].style.textUnderlineOffset = "5px";
       break;
-    case "/page/news.html":
+    case "https://www.tptrading.net/page/news.html":
       pageBtns[3].style.textDecoration = "underline";
       pageBtns[3].style.textUnderlineOffset = "5px";
       break;
-    case "/page/contact.html":
+    case "https://www.tptrading.net/page/contact.html":
       pageBtns[4].style.textDecoration = "underline";
       pageBtns[4].style.textUnderlineOffset = "5px";
       break;
