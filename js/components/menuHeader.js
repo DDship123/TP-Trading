351ed55a287d9695e41138298635054aa0ebf9ef
DDship123 <96617601+DDship123@users.noreply.github.com>
2025-07-14 12:48:24 +0700

    update url

--- js/components/menuHeader.js
+++ js/components/menuHeader.js
@@ -2,23 +2,23 @@ window.addEventListener("load", () => {
   let menuIframe = document.querySelector(".header .menu").contentDocument;
   let pageBtns = menuIframe.querySelectorAll(".menu_tags a");
   switch (window.location.href) {
-    case "http://127.0.0.1:5500/index.html":
+    case "/index.html":
       pageBtns[0].style.textDecoration = "underline";
       pageBtns[0].style.textUnderlineOffset = "5px";
       break;
-    case "http://127.0.0.1:5500/page/tradingBrokers.html":
+    case "/page/tradingBrokers.html":
       pageBtns[1].style.textDecoration = "underline";
       pageBtns[1].style.textUnderlineOffset = "5px";
       break;
-    case "http://127.0.0.1:5500/page/blog.html":
+    case "/page/blog.html":
       pageBtns[2].style.textDecoration = "underline";
       pageBtns[2].style.textUnderlineOffset = "5px";
       break;
-    case "http://127.0.0.1:5500/page/news.html":
+    case "/page/news.html":
       pageBtns[3].style.textDecoration = "underline";
       pageBtns[3].style.textUnderlineOffset = "5px";
       break;
-    case "http://127.0.0.1:5500/page/contact.html":
+    case "/page/contact.html":
       pageBtns[4].style.textDecoration = "underline";
       pageBtns[4].style.textUnderlineOffset = "5px";
       break;
